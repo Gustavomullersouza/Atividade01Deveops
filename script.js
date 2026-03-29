a85f1261bb46eb42d2d28a2ecfcb5d15f9c23f77
Gustavomullersouza <168039864+Gustavomullersouza@users.noreply.github.com>
2026-03-29 00:47:46 -0300

    Implementado sistema de mensagens com contador e data/hora

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let totalMensagens = 0;
+
 function mostrarMensagem() {
   const mensagem = document.getElementById("mensagemInput").value;
 
@@ -6,5 +8,15 @@ function mostrarMensagem() {
     return;
   }
 
-  document.getElementById("msg").innerText = "Mensagem enviada: " + mensagem;
+  totalMensagens++;
+
+  const data = new Date();
+
+  document.getElementById("msg").innerText =
+    "Mensagem enviada: " + mensagem + " (" + data.toLocaleString() + ")";
+
+  document.getElementById("contador").innerText =
+    "Mensagens enviadas: " + totalMensagens;
+
+  document.getElementById("mensagemInput").value = "";
 }
